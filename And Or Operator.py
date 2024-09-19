e23af0fb05d056e8419bf8fee90b2691d56df901
Saifan594 <saifanhasan72@gmail.com>
2024-09-19 18:31:50 +0600

    first commit

---
+++ And Or Operator.py
@@ -0,0 +1,30 @@
+print("\033c")
+
+a = 10
+b = -10
+
+# and
+
+if a > 0 and b > 0 :
+    print("both numbers are greater than 0")
+
+else :
+    print("one of the number is smaller than 0")
+
+# or
+
+if a > 0 or b > 0 :
+    print("one of the number is greater than 0")
+
+else :
+    print("both numbers are smaller than 0")
+
+# not
+
+x = None
+
+if not x :
+    print("x has no value")
+
+else :
+    print("x has value")
